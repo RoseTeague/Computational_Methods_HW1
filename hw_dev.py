c47aa56a1cf5e137a99ff46a1b91924dcd4700c0
Rosemary <rdt13@ic.ac.uk>
2017-11-02 20:48:13 +0000

    Code restructured in order to correctly compute rwnet2. Each step is now computed seperately rather than calling rw2d for a set number of time steps. This resulted in almost doubling of the computational time required to generate H=500 Hf=300 graph (Now approx 10minutes 30 rather than 5 minutes 30 as before). All graphs now saved to a higher resolution.

--- hw_dev.py
+++ hw_dev.py
@@ -17,21 +17,22 @@ def rw2d(Nt,M,a=0,b=0):
 
     start = time.time()
 
+    #assert conditions on the values of a and b
     assert a>=-1, \
         "a should not be less than -1"
     assert b<=1, \
         "b should not be greater than 1"
 
+    #set initial parameters
     X=0;Y=0;X2=0;Y2=0;XY=0
     m=np.arange(1,M+1)
 
+    #m random walks, each of length Nt, created by setting up 2 arrays of random choices 0 or 1
+    #which are then weighted by the values a and b and added cumulatively
     for i in m:
-        Choice=np.random.randint(0,2,Nt)
-        Choice2=np.random.randint(0,2,Nt)#Makes a random choice of 0 or 1 for each of Nt time steps
-        #Assign the number 0 to a step of 1+a and the number 1 to a step of 1 in X
-        # and similarly for steps in Y. Cumulatively add up these steps to create random paths
-        x=np.cumsum(Choice*(-1)+(1-Choice)*(1+a))
-        y=np.cumsum(Choice2*(-1)+(1-Choice2)*(1-b))
+        #Choice2=np.random.randint(0,2,Nt)
+        x=np.cumsum(np.random.randint(0,2,Nt)*(-2-a)+(1+a))
+        y=np.cumsum(np.random.randint(0,2,Nt)(-2+b)+(1-b))
         #Adds each successive path, M, at each timestep. All in the format of Nt dimensional arrays
         x=np.insert(x,0,0)
         y=np.insert(y,0,0)
@@ -59,6 +60,7 @@ def test(n,l,d,y,x,X,Y,BRK):
             y1=y.index(Y+l,y1+1)
             if (np.abs(X-x[y1]))<=d:
                 BRK=1
+                break
     return BRK
 
 
@@ -73,82 +75,58 @@ def rwnet1(H,Hf,a=0,display=False,analyze=False):
     output: a tuple containing any other information you would
     like the function to return, may be left empty
     """
-    #start = time.time()
+    start = time.clock()
     assert a in [-1,0,1], \
         "a should be -1, 0 or 1"
 
     plt.close()
-    M=H*10
-    D=np.sqrt(1+(1+a)**2)
-    G=nx.Graph()
-    G.add_node(0,pos=(0,0))
-    pos=nx.get_node_attributes(G,'pos')
-    l=len(pos)
-    x=[0]
-    y=[0]
+    x=[0];y=[0];t=0;X=0;Y=H;BRK=0
     ymax=[]
     tmax=[]
 
-    for m in range(M):
-        poslist=rw2d(M,1,a,1)
-
-        X=poslist[0]
-        Y=[yi+H for yi in poslist[1]]
-        BRK=0
-
-        for i in range(M+1):
-            if Y[i]==0:
-                BRK=1
-            else:
-                if a==0:
-                    n_1,n0,n1=y.count(Y[i]-1),y.count(Y[i]),y.count(Y[i]+1)
-                    BRK=test(n_1,-1,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n0,0,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n1,1,1,y,x,X[i],Y[i],BRK)
-                elif a==-1:
-                    n_1,n0,n1=y.count(Y[i]-1),y.count(Y[i]),y.count(Y[i]+1)
-                    BRK=test(n_1,-1,0,y,x,X[i],Y[i],BRK)
-                    BRK=test(n0,0,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n1,1,0,y,x,X[i],Y[i],BRK)
-                elif a==1:
-                    n_2,n_1,n0,n1,n2=y.count(Y[i]-2),y.count(Y[i]-1),y.count(Y[i]),y.count(Y[i]+1),y.count(Y[i]+2)
-                    BRK=test(n_2,-2,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n_1,-1,2,y,x,X[i],Y[i],BRK)
-                    BRK=test(n0,0,2,y,x,X[i],Y[i],BRK)
-                    BRK=test(n1,1,2,y,x,X[i],Y[i],BRK)
-                    BRK=test(n2,2,1,y,x,X[i],Y[i],BRK)
-
-            if BRK==1:
-                G.add_node(l,pos=(X[i],Y[i]))
-                if analyze==True:
-                    if Y[i]>max(y):
-                        ymax.append(Y[i])
-                        tmax.append(l)
-                x.append(X[i])
-                y.append(Y[i])
-                break
-            else:
-                continue
-
-        if y[-1]>=Hf:
-            G.remove_node(l)
-            break
-
-        pos=nx.get_node_attributes(G,'pos')
-        l=len(pos)
+    while max(y)<Hf:
+        X=X+np.random.randint(0,2)*(-2-a)+(1+a)
+        Y=Y-np.random.randint(0,2)
+        if Y<=0:
+            BRK=1
+        else:
+            if a==0:
+                n_1,n0,n1=y.count(Y-1),y.count(Y),y.count(Y+1)
+                BRK=test(n_1,-1,1,y,x,X,Y,BRK)
+                BRK=test(n0,0,1,y,x,X,Y,BRK)
+                BRK=test(n1,1,1,y,x,X,Y,BRK)
+            elif a==-1:
+                n_1,n0,n1=y.count(Y-1),y.count(Y),y.count(Y+1)
+                BRK=test(n_1,-1,0,y,x,X,Y,BRK)
+                BRK=test(n0,0,1,y,x,X,Y,BRK)
+                BRK=test(n1,1,0,y,x,X,Y,BRK)
+            elif a==1:
+                n_2,n_1,n0,n1,n2=y.count(Y-2),y.count(Y-1),y.count(Y),y.count(Y+1),y.count(Y+2)
+                BRK=test(n_2,-2,1,y,x,X,Y,BRK)
+                BRK=test(n_1,-1,2,y,x,X,Y,BRK)
+                BRK=test(n0,0,2,y,x,X,Y,BRK)
+                BRK=test(n1,1,2,y,x,X,Y,BRK)
+                BRK=test(n2,2,1,y,x,X,Y,BRK)
+        if BRK==1:
+            if analyze==True:
+                t=t+1
+                if Y>max(y):
+                    ymax.append(Y)
+                    tmax.append(t)
+            x.append(X);y.append(Y)
+            X=0;Y=H;BRK=0
 
     if display==True:
         n='H='+str(H)+',Hf='+str(Hf)+',a='+str(a)
-        #nx.draw_networkx(G,pos,node_size=6,with_labels=False)
-        plt.plot(x,y,'ro')
+        plt.plot(x,y,'ro',markersize=1)
         plt.title(n)
-        plt.savefig(n)
+        plt.savefig(n,dpi=700)
         plt.show()
 
-    #end = time.time()
-    #print(end-start)
+    end = time.clock()
+    print(end-start)
 
-    return G,pos,x,y,ymax,tmax#,output
+    return x,y,ymax,tmax
 
 def rwnet2(L,H,Hf,a=0,display=False,analyze=False):
     """Input variables
@@ -162,86 +140,70 @@ def rwnet2(L,H,Hf,a=0,display=False,analyze=False):
     output: a tuple containing any other information you would
     like the function to return, may be left empty
     """
-    #start = time.time()
+    start = time.clock()
     assert a in [-1,0,1], \
         "a should be -1, 0 or 1"
 
     plt.close()
-    M=H*10
-    D=np.sqrt(1+(1+a)**2)
-    G=nx.Graph()
-    G.add_node(0,pos=(0,0))
-    pos=nx.get_node_attributes(G,'pos')
-    l=len(pos)
-    x=[0]
-    y=[0]
+    x=[0];y=[0];t=0;X=0;Y=H;BRK=0
     ymax=[]
     tmax=[]
 
-    for m in range(M):
-        poslist=rw2d(M,1,a,1)
+    while max(y)<Hf:
+        X=X+np.random.randint(0,2)*(-2-a)+(1+a)
+        Y=Y-np.random.randint(0,2)
+        if X>=L:
+            X=L
+        elif X<=-L:
+            X=-L
 
-        X=poslist[0]
-        Y=[yi+H for yi in poslist[1]]
-        BRK=0
-
-        for i in range(M+1):
-            if np.abs(X[i])>=L:
-                X[i]=L
-
-            if Y[i]<=0:
-                BRK=1
-            else:
-                if a==0:
-                    n_1,n0,n1=y.count(Y[i]-1),y.count(Y[i]),y.count(Y[i]+1)
-                    BRK=test(n_1,-1,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n0,0,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n1,1,1,y,x,X[i],Y[i],BRK)
-                elif a==-1:
-                    n_1,n0,n1=y.count(Y[i]-1),y.count(Y[i]),y.count(Y[i]+1)
-                    BRK=test(n_1,-1,0,y,x,X[i],Y[i],BRK)
-                    BRK=test(n0,0,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n1,1,0,y,x,X[i],Y[i],BRK)
-                elif a==1:
-                    n_2,n_1,n0,n1,n2=y.count(Y[i]-2),y.count(Y[i]-1),y.count(Y[i]),y.count(Y[i]+1),y.count(Y[i]+2)
-                    BRK=test(n_2,-2,1,y,x,X[i],Y[i],BRK)
-                    BRK=test(n_1,-1,2,y,x,X[i],Y[i],BRK)
-                    BRK=test(n0,0,2,y,x,X[i],Y[i],BRK)
-                    BRK=test(n1,1,2,y,x,X[i],Y[i],BRK)
-                    BRK=test(n2,2,1,y,x,X[i],Y[i],BRK)
-
-            if BRK==1:
-                G.add_node(l,pos=(X[i],Y[i]))
-                if analyze==True:
-                    if Y[i]>max(y):
-                        ymax.append(Y[i])
-                        tmax.append(l)
-                x.append(X[i])
-                y.append(Y[i])
-                break
-            else:
-                continue
-
-        if y[-1]>=Hf:
-            #G.remove_node(l)
-            break
-
-        pos=nx.get_node_attributes(G,'pos')
-        l=len(pos)
+        if Y<=0:
+            BRK=1
+        else:
+            if a==0:
+                n_1,n0,n1=y.count(Y-1),y.count(Y),y.count(Y+1)
+                BRK=test(n_1,-1,1,y,x,X,Y,BRK)
+                BRK=test(n0,0,1,y,x,X,Y,BRK)
+                BRK=test(n1,1,1,y,x,X,Y,BRK)
+            elif a==-1:
+                n_1,n0,n1=y.count(Y-1),y.count(Y),y.count(Y+1)
+                BRK=test(n_1,-1,0,y,x,X,Y,BRK)
+                BRK=test(n0,0,1,y,x,X,Y,BRK)
+                BRK=test(n1,1,0,y,x,X,Y,BRK)
+            elif a==1:
+                n_2,n_1,n0,n1,n2=y.count(Y-2),y.count(Y-1),y.count(Y),y.count(Y+1),y.count(Y+2)
+                BRK=test(n_2,-2,1,y,x,X,Y,BRK)
+                BRK=test(n_1,-1,2,y,x,X,Y,BRK)
+                BRK=test(n0,0,2,y,x,X,Y,BRK)
+                BRK=test(n1,1,2,y,x,X,Y,BRK)
+                BRK=test(n2,2,1,y,x,X,Y,BRK)
+        if BRK==1:
+            if analyze==True:
+                t=t+1
+                if Y>max(y):
+                    ymax.append(Y)
+                    tmax.append(t)
+            x.append(X);y.append(Y)
+            X=0;Y=H;BRK=0
 
     if display==True:
         n='H='+str(H)+',Hf='+str(Hf)+',a='+str(a)+',L='+str(L)
-        plt.plot(x,y,'ro',markersize=3)
+        plt.plot(x,y,'ro',markersize=1)
         plt.title(n)
-        plt.savefig(n)
+        plt.savefig(n,dpi=700)
         plt.show()
 
-    #end = time.time()
-    #print(end-start)
+    end = time.clock()
+    print(end-start)
 
-    return G,pos,x,y,ymax,tmax
+    return x,y,ymax,tmax
 
 def plotting(x1,y1,x2,y2,L=0):
+    """Input variables
+    x1,y1 : list data for first plot
+    x2,y2 : list data for second plot to be superimposed on the first
+    L : Parameter for wall distance to be included in plot title
+    """
 
     if L==0:
         n='L=Infinity'
@@ -252,8 +214,8 @@ def plotting(x1,y1,x2,y2,L=0):
     two,=plt.plot(x2,y2,'g-')
     one.set_label('alpha=0')
     two.set_label('alpha=1')
-    plt.xlabel('time')
-    plt.ylabel('network height')
+    plt.xlabel('Time (Iteration Number)')
+    plt.ylabel('Network height')
     plt.title(n)
     plt.legend()
 
@@ -264,30 +226,31 @@ def analyze():
     Figure 1: L=infinity uses input from rwnet1, plots the trends for when a=0 and a=1 as comparison
     Figure 2: L=30 uses input from rwnet2, plots the trends for when a=0 and a=1 as comparison.
         Note that in this case, L<<Hf and so most of the graphs height will be a result of nodes 'sticking' to the Walls.
-        For the case when a=0 we don't expect any height as the
+        For the case when a=1, we therefore expect the height to build up very rapidly.
+        For the case when a=0, we expect a slower increase as the height is centered around x=0
     Figure 3: L=150 uses input from rwnet2, plots the trends for when a=0 and a-1 as comparison.
         Note in this case L=Hf and so most of the height from the graph will be a result of nodes sticking to other nodes.
     """
 
-    G,pos,x,y,ymax0i,tmax0i=rwnet1(200,150,0,display=True,analyze=True)
-    G,pos,x,y,ymax1i,tmax1i=rwnet1(200,150,1,display=True,analyze=True)
+    x,y,ymax0i,tmax0i=rwnet1(200,150,0,display=True,analyze=True)
+    x,y,ymax1i,tmax1i=rwnet1(200,150,1,display=True,analyze=True)
 
     fig1=plotting(tmax0i,ymax0i,tmax1i,ymax1i)
-    plt.savefig('hw11.png')
+    plt.savefig('hw11.png',dpi=700)
     plt.close()
 
-    G,pos,x,y,ymax0j,tmax0j=rwnet2(30,200,150,0,display=True,analyze = True)
-    G,pos,x,y,ymax1j,tmax1j=rwnet2(30,200,150,1,display=True,analyze = True)
+    x,y,ymax0j,tmax0j=rwnet2(30,200,150,0,display=True,analyze = True)
+    x,y,ymax1j,tmax1j=rwnet2(30,200,150,1,display=True,analyze = True)
 
     fig2=plotting(tmax0j,ymax0j,tmax1j,ymax1j,30)
-    plt.savefig('hw12.png')
+    plt.savefig('hw12.png',dpi=700)
     plt.close()
 
-    G,pos,x,y,ymax0k,tmax0k=rwnet2(150,200,150,0,display=True,analyze = True)
-    G,pos,x,y,ymax1k,tmax1k=rwnet2(150,200,150,1,display=True,analyze = True)
+    x,y,ymax0k,tmax0k=rwnet2(150,200,150,0,display=True,analyze = True)
+    x,y,ymax1k,tmax1k=rwnet2(150,200,150,1,display=True,analyze = True)
 
     fig3=plotting(tmax0k,ymax0k,tmax1k,ymax1k,150)
-    plt.savefig('hw13.png')
+    plt.savefig('hw13.png',dpi=700)
     plt.close()
 
     return
@@ -308,32 +271,32 @@ def network(x,y,dstar,display=False,degree=False):
 
     l=[]
     g=nx.Graph()
+    #dstar=np.sqrt(2)
 
-    dstar=np.sqrt(2)
     for j in range(len(y)):
         links=[i for i in np.arange(j+1,len(y)) if np.sqrt(np.abs(x[i]-x[j])**2+np.abs(y[i]-y[j])**2)<=dstar]
         l.append(links)
-    #print(l)
 
     for j in range(len(l)):
         if len(l[j])>0:
             g.add_node(j)
             for i in range(len(l[j])):
-                #print('j=',j,'lj=',l[j],'i=',i,'lij=',l[j][i])
                 g.add_edge(j,l[j][i])
-    #print(len(g.node))
-    #plt.plot(nx.degree_histogram(g))
+
     if display==True:
-        nx.draw_networkx(g,node_size=6,with_labels=False)
+        nx.draw_networkx(g,node_size=4,with_labels=False)
         plt.title('Connections Network')
-        plt.savefig('Network of points')
+        plt.savefig('Network of points',dpi=700)
         plt.show()
 
     if degree==True:
         D=nx.degree(g)
-        plt.plot(nx.degree_histogram(g))
+        plt.plot(nx.degree_histogram(g),'k-')
         plt.bar(np.arange(len(nx.degree_histogram(g))),nx.degree_histogram(g))
+        plt.savefig('Degree of nodes', dpi=700)
         plt.show()
+    else:
+        D=[]
 
     return g,D
 
